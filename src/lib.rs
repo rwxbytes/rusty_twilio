53590224cb7365c3529e79b02ec98b78c25ea5b7
rwxbytes <rwxbytes@proton.me>
2025-02-08 17:27:47 +0000

    ..

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,3 @@ pub mod endpoints;
 pub use client::TwilioClient;
 
 pub type Result<T> = std::result::Result<T, error::TwilioError>;
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {}
-}
