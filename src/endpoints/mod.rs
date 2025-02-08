2062fa86c21e7623c3ba906d9bb2f87abc30da51
rwxbytes <rwxbytes@proton.me>
2025-02-08 17:28:22 +0000

    feat: added endpoint mod

---
+++ src/endpoints/mod.rs
@@ -0,0 +1,73 @@
+pub mod accounts;
+pub use crate::Result;
+
+use reqwest::{Method, Response, Url};
+pub use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+pub enum RequestBody {
+    Empty,
+    Json(serde_json::Value),
+    Multipart(reqwest::multipart::Form),
+    Form(Vec<(&'static str, String)>),
+}
+
+type QueryValues = Vec<(&'static str, String)>;
+
+
+#[derive(Clone, Debug, Default)]
+pub enum Region {
+    Australia,
+    Ireland,
+    #[default]
+    UnitedStates,
+}
+
+#[allow(async_fn_in_trait)]
+pub trait TwilioEndpoint{
+    const BASE_URL: &'static str = "https://api.twilio.com";
+
+    const PATH: &'static str;
+
+    const METHOD: Method;
+
+    type ResponseBody;
+
+    fn query_params(&self) -> Option<QueryValues> {
+        None
+    }
+
+    fn path_params(&self) -> Vec<(&'static str, &str)> {
+        vec![]
+    }
+
+    fn request_body(&self) -> crate::Result<RequestBody> {
+        Ok(RequestBody::Empty)
+    }
+
+    async fn response_body(self, resp: Response) -> crate::Result<Self::ResponseBody>;
+
+    fn url(&self) -> Url {
+        let mut url = Self::BASE_URL.parse::<Url>().unwrap();
+
+        let mut path = Self::PATH.to_string();
+
+        for (placeholder, id) in self.path_params() {
+            path = path.replace(placeholder, id);
+        }
+
+        url.set_path(&path);
+
+        if let Some(query_params) = self.query_params() {
+            let query_string = query_params
+                .into_iter()
+                .map(|(k, v)| format!("{}={}", k, v))
+                .collect::<Vec<_>>()
+                .join("&");
+
+            url.set_query(Some(&query_string))
+        }
+
+        url
+    }
+}
