0bf0d6c09cb13c31d591a68ca6a3f75fe058d548
rwxbytes <rwxbytes@proton.me>
2025-02-08 17:20:37 +0000

    feat: added client mod

---
+++ src/client.rs
@@ -0,0 +1,80 @@
+use crate::endpoints::{Region, RequestBody, TwilioEndpoint};
+use crate::error::TwilioError::*;
+use crate::Result;
+use reqwest::header::CONTENT_TYPE;
+use reqwest::Method;
+
+const APPLICATION_JSON: &str = "application/json";
+
+#[derive(Clone)]
+pub struct TwilioClient {
+    inner: reqwest::Client,
+    account_sid: String,
+    auth_token: String,
+    main_api_key: Option<String>,
+    main_api_key_secret: Option<String>,
+    region: Region,
+}
+
+impl TwilioClient {
+
+    pub fn account_sid(&self) -> &str {
+        &self.account_sid
+    }
+
+    pub fn from_env() -> Result<Self> {
+        Ok(Self {
+            inner: reqwest::Client::new(),
+            account_sid: std::env::var("TWILIO_ACCOUNT_SID")
+                .map_err(|_| MissingAccountSidEnvVar)?,
+            auth_token: std::env::var("TWILIO_AUTH_TOKEN").map_err(|_| MissingAuthTokenEnvVar)?,
+            main_api_key: std::env::var("TWILIO_MAIN_API_KEY").ok(),
+            main_api_key_secret: std::env::var("TWILIO_MAIN_API_KEY_SECRET").ok(),
+            region: Region::UnitedStates,
+        })
+    }
+
+    pub fn new(account_sid: impl Into<String>, auth_token: impl Into<String>) -> Self {
+        Self {
+            inner: reqwest::Client::new(),
+            account_sid: account_sid.into(),
+            auth_token: auth_token.into(),
+            main_api_key: None,
+            main_api_key_secret: None,
+            region: Region::UnitedStates,
+        }
+    }
+
+    pub async fn hit<T: TwilioEndpoint>(&self, endpoint: T) -> Result<T::ResponseBody> {
+        //if self.region != Default::default()
+
+        let mut builder = self
+            .inner
+            .request(T::METHOD, endpoint.url())
+            .basic_auth(&self.account_sid, Some(&self.auth_token));
+
+        if matches!(T::METHOD, Method::POST | Method::PATCH) {
+            let request_body = endpoint.request_body()?;
+            builder = match request_body {
+                RequestBody::Json(json) => {
+                    builder.header(CONTENT_TYPE, APPLICATION_JSON).json(&json)
+                }
+                RequestBody::Multipart(form) => builder.multipart(form),
+                RequestBody::Form(form) => builder.form(&form),
+                RequestBody::Empty => return Err(EmptyRequestBody),
+            };
+        }
+
+        let resp = builder.send().await?;
+
+        if !resp.status().is_success() {
+            return Err(Http(resp.json().await?));
+        }
+
+        endpoint.response_body(resp).await
+    }
+
+    pub fn set_region(&mut self, region: Region) {
+        self.region = region;
+    }
+}
