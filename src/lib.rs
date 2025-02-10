d38b0c942b567eabccf8e729f7e9059d1aecda06
rwxbytes <rwxbytes@proton.me>
2025-02-10 15:46:26 +0000

    feat: added url and query mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod client;
 mod error;
 pub mod endpoints;
+pub mod url;
 
 pub use client::TwilioClient;
+pub use url::query::TwilioQuery;
 
 pub type Result<T> = std::result::Result<T, error::TwilioError>;
---
+++ src/url/query.rs
@@ -0,0 +1,102 @@
+use crate::endpoints::accounts::Status;
+use crate::endpoints::voice::call::CallStatus;
+use crate::endpoints::QueryValues;
+
+#[derive(Clone, Debug, Default)]
+pub struct TwilioQuery<T> {
+    pub params: QueryValues,
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<T> TwilioQuery<T> {
+    pub fn new() -> Self {
+        Self {
+            params: vec![],
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<T> TwilioQuery<T> {
+
+    pub fn with_page_size(mut self, page_size: u32) -> Self {
+        self.params.push(("PageSize", page_size.to_string()));
+        self
+    }
+
+    pub fn with_page(mut self, page: u32) -> Self {
+        self.params.push(("Page", page.to_string()));
+        self
+    }
+
+    pub fn with_page_token(mut self, page_token: impl Into<String>) -> Self {
+        self.params.push(("PageToken", page_token.into()));
+        self
+    }
+}
+
+pub(crate) trait ByFriendlyName {}
+
+impl<T: ByFriendlyName> TwilioQuery<T> {
+    pub fn with_friendly_name(mut self, friendly_name: impl Into<String>) -> Self {
+        self.params.push(("FriendlyName", friendly_name.into()));
+        self
+    }
+}
+
+
+pub(crate) trait AccountQueryMarker {}
+impl<T: AccountQueryMarker> TwilioQuery<T> {
+    pub fn with_status(mut self, status: Status) -> Self {
+        self.params.push(("Status", status.to_string()));
+        self
+    }
+}
+
+pub(crate) trait ByToAndFrom {}
+
+impl<T: ByToAndFrom> TwilioQuery<T> {
+    pub fn with_to(mut self, to: impl Into<String>) -> Self {
+        self.params.push(("To", to.into()));
+        self
+    }
+
+    pub fn with_from(mut self, from: impl Into<String>) -> Self {
+        self.params.push(("From", from.into()));
+        self
+    }
+}
+
+pub(crate) trait CallQueryMarker {}
+
+impl<T: CallQueryMarker> TwilioQuery<T> {
+    pub fn with_parent_call_sid(mut self, parent_call_sid: impl Into<String>) -> Self {
+        self.params.push(("ParentCallSid", parent_call_sid.into()));
+        self
+    }
+
+    pub fn with_call_status(mut self, call_status: CallStatus) -> Self {
+        self.params.push(("Status", call_status.to_string()));
+        self
+    }
+
+    /// Only include calls that started on this date.
+    /// Specify a date as YYYY-MM-DD in UTC, for example: 2009-07-06, to read only calls that started on this date.
+    /// You can also specify an inequality, such as StartTime<=YYYY-MM-DD,
+    /// to read calls that started on or before midnight of this date,
+    /// and StartTime>=YYYY-MM-DD to read calls that started on or after midnight of this date.
+    pub fn with_start_time(mut self, start_time: impl Into<String>) -> Self {
+        self.params.push(("StartTime", start_time.into()));
+        self
+    }
+
+    /// Only include calls that ended on this date.
+    /// Specify a date as YYYY-MM-DD in UTC, for example: 2009-07-06, to read only calls that ended on this date.
+    /// You can also specify an inequality, such as EndTime<=YYYY-MM-DD,
+    /// to read calls that ended on or before midnight of this date,
+    /// and EndTime>=YYYY-MM-DD to read calls that ended on or after midnight of this date.
+    pub fn with_end_time(mut self, end_time: impl Into<String>) -> Self {
+        self.params.push(("EndTime", end_time.into()));
+        self
+    }
+}
